3525ba6fd1a16047f0036dda1e851b63e128456f
Vernon Mensah <vernonmensah@gmail.com>
2017-12-22 15:50:17 -0500

    Changed indexjs to work with mocha + test file

--- index.js
+++ index.js
@@ -28,6 +28,39 @@ app.use(function (err, req, res, next) {
     res.status(500).json({ message: 'Something broke!' });
 });
 
-app.listen(8080, function () {
-    console.info(`Server listening on ${this.address().port}`);
-}).on('error', console.error);
+function runServer() {
+    const port = process.env.PORT || 8080;
+    return new Promise((resolve, reject) => {
+        server = app.listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve(server);
+        }).on('error', err => {
+            reject(err);
+        });
+    });
+}
+
+// like `runServer`, this function also needs to return a promise.
+// `server.close` does not return a promise on its own, so we manually
+// create one.
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if (err) {
+                reject(err);
+                // so we don't also call `resolve()`
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
